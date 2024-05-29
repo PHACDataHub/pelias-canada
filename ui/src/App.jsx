aaa075acb1235950fcd9b8df2a02e35b58bdd840
Geordin Raganold <91495472+GRaganold@users.noreply.github.com>
2024-05-29 13:01:33 -0400

    build(UI): ui-heading and header modification and build (#40)

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -12,31 +12,7 @@ function MainContent() {
   return (
     <>
          <Layout > 
-      <h1
-        style={{
-          color: 'white',
-          padding: '15px 0px',
-          display: 'flex',
-          marginTop: '-15px',
-          flexDirection: 'column',
-          textAlign: 'center',
-          backgroundColor: '#33465c',
-          width: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: '28px',
-        }}
-      >
-        {' '}
-        Geocoder
-      </h1>
-
-      <main
-        id="main-content"
-        style={{
-          height: '100vh',
-        }}
-      ></main>
+     
      
 </Layout>
     </>
---
+++ ui/src/pages/landingPage/Ladingpage.jsx
@@ -0,0 +1,14 @@
+
+
+export default function LandingPage() {
+
+    return (
+
+        <> 
+         <Layout>
+
+            <h1> This is the landding page</h1>
+         </Layout>
+        </>
+    )
+} 
