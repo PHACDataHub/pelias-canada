24010353351f0939e1e4aefefbd73bb532d1bba2
graganold <geordinraganold@gmail.com>
2024-07-24 15:05:31 -0400

    feature(ui): init reverse page and nopage, hide reverse page, add fallback to dom router

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,60 +1,60 @@
-import { Outlet, useLocation } from "react-router-dom";
-import { GcdsHeader, GcdsContainer } from "@cdssnc/gcds-components-react";
-import "@cdssnc/gcds-components-react/gcds.css";
-import Breadcrumb from "../components/Breadcrumb";
-import "./Layout.css";
-import TopNav from "./TopNav";
-import Footer from "./Footer";
-import { useTranslation } from "react-i18next";
+import { Outlet, useLocation } from "react-router-dom"
+import { GcdsHeader, GcdsContainer } from "@cdssnc/gcds-components-react"
+import "@cdssnc/gcds-components-react/gcds.css"
+import Breadcrumb from "../components/Breadcrumb"
+import "./Layout.css"
+import TopNav from "./TopNav"
+import Footer from "./Footer"
+import { useTranslation } from "react-i18next"
 
 export default function Layout() {
-  const { i18n } = useTranslation();
+	const { i18n } = useTranslation()
 
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
+	const changeLanguage = lng => {
+		i18n.changeLanguage(lng)
+	}
 
-  const location = useLocation();
+	const location = useLocation()
 
-  return (
-    <>
-      <GcdsHeader skipToHref="#main-content" padding="150px" height="auto" lang={i18n.language}>
-        <div slot="toggle">
-          {i18n.language === "en" ? (
-            <button className="astext" onClick={() => changeLanguage("fr")}>
-              French
-            </button>
-          ) : (
-            <button className="astext" onClick={() => changeLanguage("en")}>
-              English
-            </button>
-          )}
-        </div>
-        <div slot="skip-to-nav">
-          <a className="skip-to-content-link" href="#main-content">
-            Skip to main content
-          </a>
-        </div>
-        <nav slot="menu" style={{ backgroundColor: "#f1f2f3" }}>
-          <GcdsContainer size="xl" centered color="black">
-            <TopNav />
-          </GcdsContainer>
-        </nav>
-      </GcdsHeader>
-      <GcdsContainer
-        size={location.pathname === "/home" ||location.pathname === "/" ? "full" : "xl"}
-        centered
-        color="black"
-        style={{
-          flexGrow: "1",
-        }}
-        padding="400"
-        id="main-content"
-      >
-        <Breadcrumb />
-        <Outlet />
-      </GcdsContainer>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<GcdsHeader skipToHref="#main-content" padding="150px" height="auto" lang={i18n.language}>
+				<div slot="toggle">
+					{i18n.language === "en" ? (
+						<button className="astext" onClick={() => changeLanguage("fr")}>
+							French
+						</button>
+					) : (
+						<button className="astext" onClick={() => changeLanguage("en")}>
+							English
+						</button>
+					)}
+				</div>
+				<div slot="skip-to-nav">
+					<a className="skip-to-content-link" href="#main-content">
+						Skip to main content
+					</a>
+				</div>
+				<nav slot="menu" style={{ backgroundColor: "#f1f2f3" }}>
+					<GcdsContainer size="xl" centered color="black">
+						<TopNav />
+					</GcdsContainer>
+				</nav>
+			</GcdsHeader>
+			<GcdsContainer
+				size={location.pathname === "/home" || location.pathname === "/" ? "full" : "xl" || "lg"}
+				centered
+				color="black"
+				style={{
+					flexGrow: "1",
+				}}
+				padding="400"
+				id="main-content"
+			>
+				<Breadcrumb />
+				<Outlet />
+			</GcdsContainer>
+			<Footer />
+		</>
+	)
 }
