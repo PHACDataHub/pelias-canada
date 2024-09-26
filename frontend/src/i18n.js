58346a35f84398d2e2ea10de9c45fa65edec2f97
graganold <geordinraganold@gmail.com>
2024-09-26 08:35:50 -0400

    chore(ui): adjust language detection on application load

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,20 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector'; // Add this import
 
 i18n
   .use(HttpBackend)
+  .use(LanguageDetector) // Use the language detector
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    lng: 'en',
     backend: {
-      loadPath: '/locales/{{lng}}/translation.json'
+      loadPath: '/locales/{{lng}}/translation.json',
     },
     react: {
-      useSuspense: false
+      useSuspense: false,
     },
-    debug: false // Enable debug mode to see more detailed logs
+    debug: false, // Enable debug mode to see more detailed logs
   });
 
 export default i18n;
