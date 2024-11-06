30aea53299d7b3f106aa2786ffa3650fad82073b
Geordin Raganold <91495472+GRaganold@users.noreply.github.com>
2024-11-06 10:45:50 -0500

    chore(ui): a11y - change top nav from custom to gcds design. Legacy component transfered from pre-alpha version (#286)

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,18 +1,13 @@
 import { Outlet, useLocation } from "react-router-dom"
-import { GcdsHeader, GcdsContainer, GcdsFooter } from "@cdssnc/gcds-components-react"
+import { GcdsHeader, GcdsContainer, GcdsFooter, GcdsTopNav, GcdsNavLink, GcdsNavGroup, GcdsLangToggle, GcdsBreadcrumbsItem, GcdsBreadcrumbs } from "@cdssnc/gcds-components-react"
 import "@cdssnc/gcds-components-react/gcds.css"
-import Breadcrumb from "../components/Breadcrumb"
 import "./Layout.css"
-import TopNav from "./TopNav"
 import { useTranslation } from "react-i18next"
+import { useState } from "react"
 
 export default function Layout() {
 	const { t, i18n } = useTranslation()
-
-	const changeLanguage = lng => {
-		i18n.changeLanguage(lng)
-	}
-
+	const [announcement, setAnnouncement] = useState("") // State for announcements
 	const location = useLocation()
 
 	const contextualLinks = {
@@ -20,50 +15,97 @@ export default function Layout() {
 		[t("footer.faq")]: "/frequently-asked-questions",
 	}
 
+	const changeLanguage = lng => {
+		i18n.changeLanguage(lng)
+		setAnnouncement(`${lng === "fr" ? "La langue a été changée en français" : "The language has been changed to English"}`) // Update announcement
+	}
+
 	return (
 		<>
-			<GcdsHeader skipToHref="#main-content" padding="150px" height="auto" lang={i18n.language}>
-				<div slot="toggle">
-					{i18n.language === "en" ? (
-						<button className="astext" onClick={() => changeLanguage("fr")}>
-							French
-						</button>
-					) : (
-						<button className="astext" onClick={() => changeLanguage("en")}>
-							English
-						</button>
+			<GcdsHeader langHref={i18n.language} signatureHasLink="false" lang={i18n.language}>
+				<div slot="menu">
+					<GcdsTopNav label="Top navigation" alignment="right">
+						<GcdsNavLink href="/" slot="home">
+							Pelias
+						</GcdsNavLink>
+						<GcdsNavGroup openTrigger={t("menu.bulkFile")} menuLabel={t("menu.bulkFile")}>
+							<GcdsNavLink href="/reverse-bulk-files" current={location.pathname === "/reverse-bulk-files" ? true : undefined}>
+								{t("menu.reverseBulkFile")}
+							</GcdsNavLink>
+							<GcdsNavLink href="/forward-bulk-files" current={location.pathname === "/forward-bulk-files" ? true : undefined}>
+								{t("menu.addressBulkFile")}
+							</GcdsNavLink>
+						</GcdsNavGroup>
+						<GcdsNavGroup openTrigger={t("menu.developers")} menuLabel={t("menu.developers")}>
+							<GcdsNavLink href="/r-api" current={location.pathname === "/r-api" ? true : undefined}>
+								R Api
+							</GcdsNavLink>
+							<GcdsNavLink href="/python-api" current={location.pathname === "/python-api" ? true : undefined}>
+								Python Api
+							</GcdsNavLink>
+						</GcdsNavGroup>
+						<GcdsNavLink href="/contact-us" current={location.pathname === "/contact-us" ? true : undefined}>
+							{t("menu.contactUs")}
+						</GcdsNavLink>
+					</GcdsTopNav>
+				</div>
+				<div slot="breadcrumb">
+					{location.pathname === "/" ? null : (
+						<GcdsBreadcrumbs>
+							<GcdsBreadcrumbsItem href="/">Pelias</GcdsBreadcrumbsItem>
+						</GcdsBreadcrumbs>
 					)}
 				</div>
-				<div slot="skip-to-nav">
+				<div slot="skip-to-nav" style={{ textAlign: "center", top: "10px", left: 0, width: "100%", zIndex: 3 }}>
 					<a className="skip-to-content-link" href="#main-content">
-						Skip to main content
+						Skip to main content / Passer au contenu principal
 					</a>
 				</div>
-				<nav slot="menu" style={{ backgroundColor: "#f1f2f3" }}>
-					<GcdsContainer size="xl" centered color="black">
-						<TopNav />
-					</GcdsContainer>
-				</nav>
+				<div slot="toggle">
+					<GcdsLangToggle
+						href="#"
+						lang={i18n.language}
+						onClick={event => {
+							event.preventDefault()
+							changeLanguage(i18n.language === "en" ? "fr" : "en")
+						}}
+					></GcdsLangToggle>
+				</div>
 			</GcdsHeader>
+
 			<GcdsContainer
-				size={location.pathname === "/home" || location.pathname === "/" ? "full" : "xl" || "lg"}
+				size={location.pathname === "/home" || location.pathname === "/" ? "full" : "xl"}
 				centered
 				color="black"
-				style={{
-					flexGrow: "1",
-				}}
-				padding="400"
+				style={{ flexGrow: "1" }}
+				main-container
 				id="main-content"
 			>
-				<Breadcrumb />
 				<Outlet />
+				<GcdsTopNav
+  label="Top navigation"
+  alignment="right"
+>
+  <GcdsNavLink href="#home" slot="home">GC Notify</GcdsNavLink> 
+  <GcdsNavLink href="#">Why GC Notify</GcdsNavLink>
+
+  <GcdsNavGroup  openTrigger="Features" menuLabel="Features">
+    <GcdsNavLink href="#" current>Create reusable templates</GcdsNavLink>
+    <GcdsNavLink href="#">Personalize messages</GcdsNavLink>
+    <GcdsNavLink href="#">Schedule messages</GcdsNavLink>
+    <GcdsNavLink href="#">Send messages automatically</GcdsNavLink>
+  </GcdsNavGroup>
+
+  <GcdsNavLink href="#">Contact us</GcdsNavLink>
+</GcdsTopNav>
 			</GcdsContainer>
-			<GcdsFooter
-				lang={i18n.language}
-				display="full"				
-				contextualHeading={t("footer.additionalNav")}
-				contextualLinks={contextualLinks} // Pass the object here
-			/>
+
+			{/* Announce the language change */}
+			<span aria-live="polite" style={{ position: "absolute", left: "-9999px", width: "1px", height: "1px", overflow: "hidden" }}>
+				{announcement}
+			</span>
+
+			<GcdsFooter lang={i18n.language} display="full" contextualHeading={t("footer.additionalNav")} contextualLinks={contextualLinks} style={{paddingTop:"50px"}}/>
 		</>
 	)
 }
