c97a9d2eabd34720386b1a1f45609fb0d2988a37
Geordin Raganold <91495472+GRaganold@users.noreply.github.com>
2024-11-15 10:11:38 -0500

    fix(ui): a11y - erros handling and translations updated (#299)

--- frontend/src/pages/contactUs/ContactUs.jsx
+++ frontend/src/pages/contactUs/ContactUs.jsx
@@ -1,84 +1,114 @@
-import { useState } from "react"
-import { GcdsHeading , GcdsButton} from "@cdssnc/gcds-components-react"
+import { useState, useEffect } from "react"
+import { GcdsInput, GcdsTextarea, GcdsButton, GcdsHeading, GcdsSelect, GcdsText } from "@cdssnc/gcds-components-react"
 import { useTranslation } from "react-i18next"
-import "./contactUs.css" // Import the CSS file
 
 export default function ContactUs() {
-	const { t } = useTranslation()
-	const [submitted, setSubmitted] = useState(false) // State to track form submission
+	const { t, i18n } = useTranslation()
+	const [submitted, setSubmitted] = useState(false)
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		subject: "",
+		message: "",
+	})
+
+	const handleInputChange = event => {
+		const { name, value } = event.target
+		setFormData({ ...formData, [name]: value })
+	}
 
 	const handleSubmit = event => {
 		event.preventDefault()
 
-		const form = event.target
-		const name = form.name.value
-		const email = form.email.value
-		const subject = form.subject.value
-		const message = form.message.value
+		// Check if all required fields have values
+		const isValid = formData.name && formData.email && formData.subject && formData.message
 
-		const mailtoLink = `mailto:Geordin.Raganold@hc-sc.gc.ca?subject=${encodeURIComponent(subject+ " - Pelias Geocoder")}&body=${encodeURIComponent(t("pages.contactUs.name"))}: ${encodeURIComponent(name)}%0A%0A${encodeURIComponent(t("pages.contactUs.email"))}: ${encodeURIComponent(email)} %0A%0A${encodeURIComponent(t("pages.contactUs.subject.title"))}: ${encodeURIComponent(subject)}%0A%0A${encodeURIComponent(t("pages.contactUs.message"))}: %0A%0A${encodeURIComponent(message)}`
+		if (isValid) {
+			const mailtoLink = `mailto:Geordin.Raganold@hc-sc.gc.ca?subject=${encodeURIComponent(formData.subject)}&body=${encodeURIComponent(
+				`${t("pages.contactUs.name")}:\n${formData.name}\n\n${t("pages.contactUs.email")}:\n${formData.email}\n\n${t("pages.contactUs.subject.title")}:\n${formData.subject}\n\n${t("pages.contactUs.message")}:\n${formData.message}`
+			)}`
 
-		window.location.href = mailtoLink
-
-		setSubmitted(true) // Update state to show thank you message
+			window.location.href = mailtoLink
+			setSubmitted(true)
+		}
 	}
 
+	// Reset form when the language changes
+	useEffect(() => {
+		setFormData({
+			name: "",
+			email: "",
+			subject: "",
+			message: "",
+		})
+		setSubmitted(false)
+	}, [i18n.language])
+
 	return (
-		<div>
+		<>
 			<GcdsHeading tag="h1">{t("pages.contactUs.title")}</GcdsHeading>
 
 			{submitted ? (
-				<div className="thank-you-message">
-					<h2>{t("pages.contactUs.thankYou.title")}</h2>
+				<div style={{ padding: "20px", marginTop: "20px" }}>
+					<GcdsHeading tag="h2">{t("pages.contactUs.thankYou.title")}</GcdsHeading>
 					<p>{t("pages.contactUs.thankYou.message")}</p>
 				</div>
 			) : (
-				<form onSubmit={handleSubmit} aria-labelledby="contact-form-heading">
-					{/* Heading for form */}
-					<h2 id="contact-form-heading" className="visually-hidden">
-						{t("pages.contactUs.title")}
-					</h2>
+				<div>
+					<GcdsText characterLimit="false">
+						{t("pages.contactUs.subTitle")} <a href="mailto:Geordin.Raganold@hc-sc.gc.ca">Geordin.Raganold@hc-sc.gc.ca</a>.
+					</GcdsText>
+					<hr style={{ margin: "50px 0" }} />
+					<form onSubmit={handleSubmit} key={i18n.language}>
+						{" "}
+						{/* Adding key to trigger re-render */}
+						<GcdsHeading tag="h2">{t("pages.contactUs.formTitle")}</GcdsHeading>
+						<br />
+						<GcdsInput label={t("pages.contactUs.name")} required type="text" id="name" name="name" value={formData.name} onGcdsChange={handleInputChange} lang={i18n.language} />
+						<GcdsInput
+							label={t("pages.contactUs.email")}
+							required
+							type="email"
+							id="email"
+							name="email"
+							value={formData.email}
+							onGcdsChange={handleInputChange}
+							lang={i18n.language}
+						/>
+						<GcdsSelect
+							selectId="select-props"
+							label={t("pages.contactUs.subject.title")}
+							
+							name="subject"
+							hint={t("pages.contactUs.subject.hintMessage")}
+							defaultValue="Select option."
+							value={formData.subject}
+							onGcdsChange={handleInputChange}
+							required
+							lang={i18n.language}
+							aria-label={t("pages.contactUs.subject.hintMessage")}
+						>
+							<option value={t("pages.contactUs.subject.feedback")}>{t("pages.contactUs.subject.feedback")}</option>
+							<option value={t("pages.contactUs.subject.bug")}>{t("pages.contactUs.subject.bug")}</option>
+							<option value={t("pages.contactUs.subject.helpOption")}>{t("pages.contactUs.subject.helpOption")}</option>
+							<option value={t("pages.contactUs.subject.inquire")}>{t("pages.contactUs.subject.inquire")}</option>
+							<option value={t("pages.contactUs.subject.followingUp")}>{t("pages.contactUs.subject.followingUp")}</option>
+						</GcdsSelect>
+						<GcdsTextarea
+							label={t("pages.contactUs.message")}
+							required
+							id="message"
+							name="message"
+							value={formData.message}
+							onGcdsChange={handleInputChange}
+							lang={i18n.language}
+						/>
+						<GcdsButton type="submit">{t("pages.contactUs.submitButton")}</GcdsButton>
+					</form>
 
-					{/* Name Field */}
-					<label htmlFor="name" className="form-label">
-						{t("pages.contactUs.name")}: <strong className="required">(required)</strong>
-					</label>
-					<input type="text" id="name" name="name" required aria-required="true" className="form-input" />
-
-					{/* Email Field */}
-					<label htmlFor="email" className="form-label">
-						{t("pages.contactUs.email")}: <strong className="required">(required)</strong>
-					</label>
-					<input type="email" id="email" name="email" required aria-required="true" className="form-input" />
-
-					{/* Subject Field */}
-					<label htmlFor="subject" className="form-label">
-						{t("pages.contactUs.subject.title")}: <strong className="required">(required)</strong>
-					</label>
-					<select id="subject" name="subject" required aria-required="true" className="form-select" defaultValue={""}>
-						<option value="" disabled selected>
-							{t("pages.contactUs.subject.select")}
-						</option>
-						<option value={t("pages.contactUs.subject.feedback")}>{t("pages.contactUs.subject.feedback")}</option>
-						<option value={t("pages.contactUs.subject.bug")}>{t("pages.contactUs.subject.bug")}</option>
-						<option value={t("pages.contactUs.subject.helpOption")}>
-							{t("pages.contactUs.subject.helpOption")}</option>
-						<option value={t("pages.contactUs.subject.inquire")}>{t("pages.contactUs.subject.inquire")}</option>
-						<option value={t("pages.contactUs.subject.followingUp")}>{t("pages.contactUs.subject.followingUp")}</option>
-					</select>
-
-					{/* Message Field */}
-					<label htmlFor="message" className="form-label">
-						{t("pages.contactUs.message")}: <strong className="required">(required)</strong>
-					</label>
-					<textarea id="message" name="message" required aria-required="true" className="form-textarea-message" />
-
-					{/* Submit Button */}
-					<GcdsButton type="submit" className="form-submit-button">
-						{t("pages.contactUs.submitButton")}
-					</GcdsButton>
-				</form>
+		
+				</div>
 			)}
-		</div>
+		</>
 	)
 }
