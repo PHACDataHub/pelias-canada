425e4c17fcba6c8325427c41f4e850eed78f3fe4
graganold <geordinraganold@gmail.com>
2024-12-05 11:11:43 -0500

    choure(ui): updates to geo results exp page

--- frontend/src/pages/geocodingExplanation/GeocodingExplanation.jsx
+++ frontend/src/pages/geocodingExplanation/GeocodingExplanation.jsx
@@ -1,215 +1,186 @@
 import { useTranslation } from "react-i18next"
 import "./GeocodingExplanation.css"
-import { GcdsHeading } from "@cdssnc/gcds-components-react"
+import { GcdsHeading, GcdsText } from "@cdssnc/gcds-components-react"
 
 export default function GeocodingExplanation() {
+	const featuresData = [
+		[
+			{ key: "$.features", description: "pages.geocodingExplanation.apiFeature.features" },
+			{ key: "$.features[0].bbox", description: "pages.geocodingExplanation.apiFeature.bbox" },
+			{ key: "$.features[0].geometry.coordinates", description: "pages.geocodingExplanation.apiFeature.geometry.coordinates" },
+			{ key: "$.features[0].geometry.type", description: "pages.geocodingExplanation.apiFeature.geometry.type" },
+			{ key: "$.features[0].type", description: "pages.geocodingExplanation.apiFeature.type" },
+			{ key: "$.features[0].properties", description: "pages.geocodingExplanation.apiFeature.properties.properties" },
+			{ key: "$.features[0].properties.country", description: "pages.geocodingExplanation.apiFeature.properties.country" },
+			{ key: "$.features[0].properties.gid", description: "pages.geocodingExplanation.apiFeature.properties.gid" },
+			{ key: "$.features[0].properties.locality_gid", description: "pages.geocodingExplanation.apiFeature.properties.locality_gid" },
+			{ key: "$.features[0].properties.neighbourhood_gid", description: "pages.geocodingExplanation.apiFeature.properties.neighbourhood_gid" },
+			{ key: "$.features[0].properties.confidence", description: "pages.geocodingExplanation.apiFeature.properties.confidence" },
+			{ key: "$.features[0].properties.country_a", description: "pages.geocodingExplanation.apiFeature.properties.country_a" },
+			{ key: "$.features[0].properties.county", description: "pages.geocodingExplanation.apiFeature.properties.county" },
+			{ key: "$.features[0].properties.locality", description: "pages.geocodingExplanation.apiFeature.properties.locality" },
+			{ key: "$.features[0].properties.accuracy", description: "pages.geocodingExplanation.apiFeature.properties.accuracy" },
+			{ key: "$.features[0].properties.source", description: "pages.geocodingExplanation.apiFeature.properties.source" },
+			{ key: "$.features[0].properties.label", description: "pages.geocodingExplanation.apiFeature.properties.label" },
+			{ key: "$.features[0].properties.region_a", description: "pages.geocodingExplanation.apiFeature.properties.region_a" },
+			{ key: "$.features[0].properties.layer", description: "pages.geocodingExplanation.apiFeature.properties.layer" },
+			{ key: "$.features[0].properties.country_code", description: "pages.geocodingExplanation.apiFeature.properties.country_code" },
+			{ key: "$.features[0].properties.street", description: "pages.geocodingExplanation.apiFeature.properties.street" },
+			{ key: "$.features[0].properties.neighbourhood", description: "pages.geocodingExplanation.apiFeature.properties.neighbourhood" },
+			{ key: "$.features[0].properties.region_gid", description: "pages.geocodingExplanation.apiFeature.properties.region_gid" },
+			{ key: "$.features[0].properties.name", description: "pages.geocodingExplanation.apiFeature.properties.name" },
+			{ key: "$.features[0].properties.match_type", description: "pages.geocodingExplanation.apiFeature.properties.match_type" },
+			{ key: "$.features[0].properties.id", description: "pages.geocodingExplanation.apiFeature.properties.id" },
+			{ key: "$.features[0].properties.source_id", description: "pages.geocodingExplanation.apiFeature.properties.source_id" },
+			{ key: "$.features[0].properties.country_gid", description: "pages.geocodingExplanation.apiFeature.properties.country_gid" },
+			{ key: "$.features[0].properties.region", description: "pages.geocodingExplanation.apiFeature.properties.region" },
+			{ key: "$.features[0].properties.county_gid", description: "pages.geocodingExplanation.apiFeature.properties.county_gid" },
+		],
+	]
 	const { t } = useTranslation()
 	return (
 		<div className="geocoding-explanation">
-      <GcdsHeading tag="h1">{t("pages.geocodingExplanation.title")}</GcdsHeading>
-			<section aria-labelledby="match-types">
-				<h2 id="match-types">{t("pages.geocodingExplanation.underTitleHeader")}</h2>
-				<p>{t("pages.geocodingExplanation.underTitlePara")}</p>
-				<h3>{t("pages.geocodingExplanation.matchTypes.title")}</h3>
+			<GcdsHeading tag="h1">{t("pages.geocodingExplanation.title")}</GcdsHeading>
+			<section>
+				<GcdsHeading tag="h2" characterLimit="false" id="match-types">
+					{t("pages.geocodingExplanation.underTitleHeader")}
+				</GcdsHeading>
+				<GcdsText characterLimit="false"> {t("pages.geocodingExplanation.underTitlePara")}</GcdsText>
+				<GcdsHeading tag="h3" characterLimit="false">
+					{t("pages.geocodingExplanation.matchTypes.title")}
+				</GcdsHeading>
 				<ul>
 					<li>
-						<strong>{t("pages.geocodingExplanation.matchTypes.exact")}</strong>:{t("pages.geocodingExplanation.matchTypes.exactPara")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.matchTypes.exact")}</strong>:{t("pages.geocodingExplanation.matchTypes.exactPara")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.matchTypes.centroid")}</strong>:{t("pages.geocodingExplanation.matchTypes.centroidPara")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.matchTypes.centroid")}</strong>:{t("pages.geocodingExplanation.matchTypes.centroidPara")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.matchTypes.interpolation")}</strong>: {t("pages.geocodingExplanation.matchTypes.interpolationPara")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.matchTypes.interpolation")}</strong>: {t("pages.geocodingExplanation.matchTypes.interpolationPara")}
+						</GcdsText>
 					</li>
 				</ul>
 			</section>
-
-			<section aria-labelledby="improving-accuracy">
-				<h2 id="improving-accuracy">{t("pages.geocodingExplanation.improvingAccuracy.title")}</h2>
-				<p>{t("pages.geocodingExplanation.improvingAccuracy.underTitle")}:</p>
+			<section>
+				<GcdsHeading tag="h2" characterLimit="false" id="improving-accuracy">
+					{t("pages.geocodingExplanation.improvingAccuracy.title")}
+				</GcdsHeading>
+				<GcdsText characterLimit="false">{t("pages.geocodingExplanation.improvingAccuracy.underTitle")}:</GcdsText>
 				<ol>
 					<li>
-						<strong>{t("pages.geocodingExplanation.improvingAccuracy.verifyAddress.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.verifyAddress.para")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.improvingAccuracy.verifyAddress.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.verifyAddress.para")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.improvingAccuracy.spellingAbbrevs.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.spellingAbbrevs.para")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.improvingAccuracy.spellingAbbrevs.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.spellingAbbrevs.para")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.improvingAccuracy.details.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.details.para")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.improvingAccuracy.details.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.details.para")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.improvingAccuracy.splitIntoComponents.title")}:</strong>{" "}
-						{t("pages.geocodingExplanation.improvingAccuracy.splitIntoComponents.para")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.improvingAccuracy.splitIntoComponents.title")}:</strong>
+							{t("pages.geocodingExplanation.improvingAccuracy.splitIntoComponents.para")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.improvingAccuracy.billingualReqs.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.billingualReqs.para")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.improvingAccuracy.billingualReqs.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.billingualReqs.para")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.improvingAccuracy.standardDigraphs.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.standardDigraphs.para")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.improvingAccuracy.standardDigraphs.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.standardDigraphs.para")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.improvingAccuracy.specialChars.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.specialChars.para")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.improvingAccuracy.specialChars.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.specialChars.para")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.improvingAccuracy.capitilization.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.capitilization.para")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.improvingAccuracy.capitilization.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.capitilization.para")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.improvingAccuracy.addressVars.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.addressVars.para")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.improvingAccuracy.addressVars.title")}:</strong> {t("pages.geocodingExplanation.improvingAccuracy.addressVars.para")}
+						</GcdsText>
 					</li>
 				</ol>
 			</section>
 
-			<section aria-labelledby="less-accurate-matches">
-				<h2 id="less-accurate-matches">{t("pages.geocodingExplanation.lessAccurateMatch.title")}</h2>
-				<p>{t("pages.geocodingExplanation.lessAccurateMatch.underTitle")}</p>
+			<section>
+				<GcdsHeading tag="h2" characterLimit="false" id="less-accurate-matches">
+					{t("pages.geocodingExplanation.lessAccurateMatch.title")}
+				</GcdsHeading>
+				<GcdsText characterLimit="false"> {t("pages.geocodingExplanation.lessAccurateMatch.underTitle")}</GcdsText>
 				<ul>
 					<li>
-						<strong>{t("pages.geocodingExplanation.lessAccurateMatch.spatialAccuracy")}</strong>: {t("pages.geocodingExplanation.lessAccurateMatch.spatialAccuracyPara")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.lessAccurateMatch.spatialAccuracy")}</strong>: {t("pages.geocodingExplanation.lessAccurateMatch.spatialAccuracyPara")}
+						</GcdsText>
 					</li>
 					<li>
-						<strong>{t("pages.geocodingExplanation.lessAccurateMatch.limitedSpatialAccuracy")}</strong>:{" "}
-						{t("pages.geocodingExplanation.lessAccurateMatch.limitedSpatialAccuracyPAra")}
+						<GcdsText characterLimit="false">
+							<strong>{t("pages.geocodingExplanation.lessAccurateMatch.limitedSpatialAccuracy")}</strong>:
+							{t("pages.geocodingExplanation.lessAccurateMatch.limitedSpatialAccuracyPAra")}
+						</GcdsText>
 					</li>
 				</ul>
 			</section>
 
-			<section aria-labelledby="geocoding-results">
-  <h2 id="geocoding-results">{t("pages.geocodingExplanation.understandingGeocodingResults")}</h2>
-  <p>{t("pages.geocodingExplanation.understandingGeocodingResultsPara")}</p>
-
-  <h3>{t("pages.geocodingExplanation.apiFeature.title")}</h3>
-  <table id="features">
-    <thead>
-      <tr>
-        <th>{t("pages.geocodingExplanation.property")}</th>
-        <th>{t("pages.geocodingExplanation.description")}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td><code>$.features</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.features")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].bbox</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.bbox")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].geometry.coordinates</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.geometry.coordinates")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].geometry.type</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.geometry.type")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].type</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.type")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.country</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.country")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.gid</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.gid")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.locality_gid</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.locality_gid")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.neighbourhood_gid</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.neighbourhood_gid")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.confidence</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.confidence")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.country_a</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.country_a")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.county</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.county")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.locality</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.locality")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.accuracy</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.accuracy")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.source</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.source")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.label</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.label")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.region_a</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.region_a")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.layer</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.layer")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.country_code</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.country_code")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.street</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.street")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.neighbourhood</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.neighbourhood")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.region_gid</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.region_gid")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.name</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.name")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.match_type</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.match_type")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.id</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.id")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.source_id</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.source_id")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.country_gid</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.country_gid")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.region</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.region")}</td>
-      </tr>
-      <tr>
-        <td><code>$.features[0].properties.county_gid</code></td>
-        <td>{t("pages.geocodingExplanation.apiFeature.properties.county_gid")}</td>
-      </tr>
-    </tbody>
-  </table>
-</section>
-
+			<section>
+				<GcdsHeading tag="h2" characterLimit="false">
+					{t("pages.geocodingExplanation.apiFeature.title")}
+				</GcdsHeading>
+				<div className="table-wrapper" tabIndex="0">
+					<table id="features" aria-label={t("pages.geocodingExplanation.apiFeature.tableTitle")}>
+						<thead>
+							<tr>
+								<th>
+									<GcdsText characterLimit="false" textRole="light">
+										{t("pages.geocodingExplanation.property")}
+									</GcdsText>
+								</th>
+								<th>
+									<GcdsText textRole="light" characterLimit="false">
+										{t("pages.geocodingExplanation.description")}
+									</GcdsText>
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							{featuresData[0].map((feature, index) => (
+								<tr key={index + feature.key}>
+									<td>
+										<GcdsText textRole="primary" characterLimit="false" marginBottom="0">
+											<code>{feature.key}</code>
+										</GcdsText>
+									</td>
+									<td>
+										<GcdsText textRole="primary" characterLimit="false" marginBottom="0">
+											{t(feature.description)}
+										</GcdsText>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</section>
 		</div>
 	)
 }
